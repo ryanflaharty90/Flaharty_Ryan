4d4e5945d044206d424cdefd75f64a0e39d5e39b
ryanflaharty90 <raflaharty@gmail.com>
2016-09-06 09:55:31 -0700

    Added Files

---
+++ Programming lesson 1/asciiart2 (2).py
@@ -0,0 +1,15 @@
+print("    _____  ")
+print("^..^     \9  ")
+print("(00)_____/   ")
+print("   ww  ww    ")
+
+print(" |\/\/\/| ")
+print(" |      | ")
+print(" |      | ")
+print(" | (o)(o) ")
+print(" C      _)")
+print("  | ,__| ")
+print("  |   /  ")
+print(" / __ \  ")
+
+      
