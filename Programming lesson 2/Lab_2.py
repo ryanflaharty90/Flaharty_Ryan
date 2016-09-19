83d0d7331f44f1b524e7bab45f5c326bc8e9a901
ryanflaharty90 <raflaharty@gmail.com>
2016-09-19 09:45:54 -0700

    new files

---
+++ Programming lesson 2/Lab_2.py
@@ -0,0 +1,25 @@
+x = 1
+y = 1
+z = x * y
+
+print("the number of shots harambe took was",z)
+
+
+      
+      
+n = 13050
+z = 92130
+
+print(" My address is", n ," cadencia place in San Diego California") 
+print(" The zip code for Carmel Valley is",z,)
+
+
+
+l = 5
+w = 9
+h = 11
+wl = w * l
+hl = h * l
+hw = h * w
+surfaceArea = 2*(wl+hl+hw)
+print("the surface area of the rectange is", surfaceArea)
---
+++ Programming lesson 3/lesson3.py
@@ -0,0 +1,8 @@
+artist = input("who is your favorite musician? ")
+
+print(artist, "?!! He sucks!")
+
+num1 = int (input("please enter your age: " ))
+num2 = int (input("please enter your moms age: "))
+
+print("Wow! She was " , num2-num1, "when she had you.")
