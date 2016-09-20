c48bd6205c84837ee205a2fe6eec2a4faa62a8d6
ryanflaharty90 <raflaharty@gmail.com>
2016-09-20 10:21:08 -0700

    new changes

---
+++ Programming lesson 3/LAB_03.py
@@ -0,0 +1,25 @@
+name = input("What is your name")
+print(name, "? Thats a horrible name")
+age = input(" How old are you Ryan? ")
+print(" wow" ,age, "Bet you still cant drive")
+fun = input(" What do you do for fun?")
+print(fun, "Dosent look like you are getting any better")
+siblings = int(input("How many siblings do you have"))
+print("Who has" ,siblings, "kids?")
+job = input("What do you want to be when you grow up?")
+print("Sorry, being a" ,job, "requires actual intelligence")
+
+
+
+
+height = int(input("whats your height?"))
+weight = int(input("Ok, and whats your weight?"))
+bmi = 703 * weight/height**2
+print("Your BMI is" ,bmi)
+
+radius = int(input("What is the radius of the shpere?"))
+sphere = 4 * 3.1415 * radius**2
+print(" The surface area of this shpere is" ,sphere)
+
+
+
