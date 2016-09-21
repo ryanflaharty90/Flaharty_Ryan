72adb057ce08d1f5b4675aaa161483ce5fca36d1
ryanflaharty90 <raflaharty@gmail.com>
2016-09-21 10:25:22 -0700

    new

---
+++ Programming lesson 4/lesson4pt1.py
@@ -0,0 +1,18 @@
+
+
+
+def printf(word, number):
+    print("{:<6}\t{:10.2f}".format(word, number))
+
+
+word = "blah!"
+number = 69789.312
+
+printf(word, number)
+
+word = "ohboy!"
+number = 8879653
+
+printf(word, number)
+    
+
