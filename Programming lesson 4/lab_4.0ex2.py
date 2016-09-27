cdc98f910e46fadcce4e97892c946b8d200468be
ryanflaharty90 <raflaharty@gmail.com>
2016-09-27 10:23:25 -0700

    new

--- Programming lesson 4/lab_4.0ex2.py
+++ Programming lesson 4/lab_4.0ex2.py
@@ -1,2 +1,16 @@
-def printf(school, year)
-    print("*{:<20}{:>10.2f}".format(school, year))
+def printf(item1, item2):
+    print("*{:<20}{:>10.2f}".format(item1, item2))
+    
+
+fname = input("What is your first name?")
+lname = input("What is your last name?")
+title = input("What is your occupation at this school?")
+school = input("What school will you be attending?")
+year = input("What school year are you in?")
+subject = input("What is your subject?")
+
+print("*" *20)
+
+printf(school, year)
+printf(fname, lname)
+printf(title, subject)
