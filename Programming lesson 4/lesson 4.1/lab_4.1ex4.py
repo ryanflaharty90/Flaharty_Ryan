108dc11d128c9f198595f50a0dac5fc797759567
ryanflaharty90 <raflaharty@gmail.com>
2016-10-04 10:20:44 -0700

    new

---
+++ Programming lesson 4/lesson 4.1/lab_4.1ex4.py
@@ -0,0 +1,9 @@
+def printf(volume):
+    print("{4.2f}".format(volume))
+
+def volBox(
+
+
+h = int(input("What is the height?"))
+l = int(input("What is the length?"))
+w = int(input("What is the width?"))
