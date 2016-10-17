88f1d7e3b0c1342fb90deb230ddfe3a262619c7a
ryanflaharty90 <raflaharty@gmail.com>
2016-10-17 10:22:16 -0700

    new

--- Programming lesson 4/Lesson_04.3/Lab_04.3(circle).py
+++ Programming lesson 4/Lesson_04.3/Lab_04.3(circle).py
@@ -1,1 +1,12 @@
+area = 0
 R= float(input("What is the radius of the circle?"))
+
+def calcArea(r):
+    global area;
+    area = float(3.1415*(r**2));
+
+def printf():
+    print("The area of the circle is {:5.5f}".format(area))
+
+calcArea(R)
+printf()
