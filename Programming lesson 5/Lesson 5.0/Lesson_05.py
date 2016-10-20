a6dd6a72fe7939020814fc4d6d1b4682a793f0a9
ryanflaharty90 <raflaharty@gmail.com>
2016-10-20 10:22:47 -0700

    new

---
+++ Programming lesson 5/Lesson 5.0/Lesson_05.py
@@ -0,0 +1,13 @@
+word1 = input("Please enter your word: ")
+word2 = "handsome"
+
+same = word1 == word2
+print (same) 
+
+
+if same:
+    print("they are the same!")
+
+if not same:
+    print("mismatch!")
+
---
+++ Programming lesson 5/Lesson 5.0/Lesson_05pt2.py
@@ -0,0 +1,13 @@
+import random
+
+num = random.randint(1, 10)
+
+
+print("The value of num: ",num)
+
+
+if num == 5:
+    print("num equals 5")
+
+if num <= 3:
+    print("num does not equal 5")
