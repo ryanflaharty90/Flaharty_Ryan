8288413ab4663d7fb86c7e7f03c42fb8ca09a558
ryanflaharty90 <raflaharty@gmail.com>
2016-10-31 10:20:59 -0700

    new

--- Programming lesson 5/Lesson 5.1/Lab_5.1ex2.py
+++ Programming lesson 5/Lesson 5.1/Lab_5.1ex2.py
@@ -6,20 +6,23 @@ def bmi(h,w):
 
 print(bmi(h,w))
 
-if bmi < 18.5:
-    print("You are underweight")
+if bmi(h,w) < 18.5:
+    condition = "You are underweight"
 
-if 18.5 < bmi < 24.9:
-    print("You have normal BMI")
+elif bmi(h,w) < 25:
+    condition = "You have normal BMI"
 
-if 25 <= bmi <= 29.9:
-    print("You are a bit overweight")
+elif bmi(h,w) < 29.9:
+    condition = "You are a bit overweight"
+    
+elif bmi(h,w) < 34.9:
+    condition = "Maybe some cardio?"
 
-if 30 <= bmi <= 34.9:
-    print("Dude hit the gym")
+elif bmi(h,w) < 39.9:
+    condition = "Cut the junk food"
 
-if 35 <= bmi <= 39.9:
-    print("Cut the junk food")
+else:
+    condition = "You need help"
 
-if 40 <= bmi:
-    print(":(")
+
+print(condition)
