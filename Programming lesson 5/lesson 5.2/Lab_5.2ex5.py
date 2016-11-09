a5acc34c80bb243ade03168527cfa9df10984be9
ryanflaharty90 <raflaharty@gmail.com>
2016-11-09 09:46:28 -0800

    new

--- Programming lesson 5/lesson 5.2/Lab_5.2ex5.py
+++ Programming lesson 5/lesson 5.2/Lab_5.2ex5.py
@@ -1,4 +1,30 @@
-def adventure():
-    choice = input("Go to forest, desert, or tundra?")
-    if choice == "forest" or choice == "desert":
-        if choice 
+print("You decide to explore the world...")
+choice = input("Visit europe or south america(SA)?")
+europe = input("Visit England, or Germany?")
+england = input("Visit london or liverpool?")
+SA = input("Visit Peru or Brazil?")
+
+if choice == europe:
+    if europe == England:
+        if choice == b:
+            #end of program
+        else:
+            #end of program
+    else:
+        if choice == c:
+            #end of program
+        else:
+            #end of program
+
+else:
+    if choice == south america:
+        if choice == b:
+            #end of program
+        else:
+            #end of program
+    else:
+        if choice == c:
+            #end of program
+        else:
+            #end of program:
+            
